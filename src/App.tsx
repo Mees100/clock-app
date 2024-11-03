4fc1d3aeea880ae58e9084c55650f85ac51ea4ff
Alina <alina100chuk@gmail.com>
2024-11-03 13:02:11 +0100

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,9 @@
-import { useEffect, useState } from "react";
-import "./App.css";
-
-// zie object literal type linksonder op https://www.typescriptlang.org/static/TypeScript%20Types-ae199d69aeecf7d4a2704a528d0fd3f9.png
-type CurrentTime = {
-  dateTime: string;
-};
-
-// een promise, gebruik je bij asynchrone code, je hebt ook een nieuwere syntax (async/await) hiervoor maar die heb ik bewust nog even weggelaten :)
-function getTime(zone: string): Promise<CurrentTime> {
-  const request = fetch(
-    `https://www.timeapi.io/api/time/current/zone?timeZone=Europe%2F${zone}`
-  );
-  return request.then((response) => response.json());
-}
+import Time from "./Time";
 
 function App() {
-  const [time, setTime] = useState<CurrentTime>();
-  const [zone, setZone] = useState<string>("Amsterdam");
-
-  useEffect(() => {
-    getTime(zone).then((r) => setTime(r));
-  }, [zone]);
-
   return (
     <>
-      {typeof time === "undefined" && <span>Loading</span>}
-      {time && <span>Het is {time.dateTime}</span>}
-      <button onClick={() => setZone("London")}>London</button>
-      <button onClick={() => setZone("Amsterdam")}>Amsterdam</button>
-      <button onClick={() => setZone("Helsinki")}>Helsinki</button>
+      <Time />
     </>
   );
 }
---
+++ src/Time.tsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+
+// zie object literal type linksonder op https://www.typescriptlang.org/static/TypeScript%20Types-ae199d69aeecf7d4a2704a528d0fd3f9.png
+type CurrentTime = {
+  dateTime: string;
+};
+
+// een promise, gebruik je bij asynchrone code, je hebt ook een nieuwere syntax (async/await) hiervoor maar die heb ik bewust nog even weggelaten :)
+function getTime(zone: string): Promise<CurrentTime> {
+  const request = fetch(
+    `https://www.timeapi.io/api/time/current/zone?timeZone=Europe%2F${zone}`
+  );
+  return request.then((response) => response.json());
+}
+
+function Time() {
+  const [time, setTime] = useState<CurrentTime>();
+  const [zone, setZone] = useState<string>("Amsterdam");
+
+  useEffect(() => {
+    getTime(zone).then((r) => setTime(r));
+  }, [zone]);
+
+  return (
+    <>
+      {typeof time === "undefined" && <span>Loading</span>}
+      {time && <span>Het is {time.dateTime}</span>}
+      <button onClick={() => setZone("London")}>London</button>
+      <button onClick={() => setZone("Amsterdam")}>Amsterdam</button>
+      <button onClick={() => setZone("Helsinki")}>Helsinki</button>
+    </>
+  );
+}
+
+export default Time;
