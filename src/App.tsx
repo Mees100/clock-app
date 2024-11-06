1958bb344ed3e1772e99b7b53716215ab2d4a34f
Alina <alina100chuk@gmail.com>
2024-11-06 15:16:46 +0100

    install fluent ui

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,9 @@ import { useState } from "react";
 import Time from "./Time";
 import TimeZone from "./TimeZone";
 
+// volgende keer maken we het mooi: https://react.fluentui.dev/?path=/docs/concepts-developer-quick-start--docs
 function App() {
-  // Dit is een voorbeeld van lifting state up, zie https://react.dev/learn/sharing-state-between-components#lifting-state-up-by-example
+  // Leren (advanced) Dit is een voorbeeld van lifting state up, zie https://react.dev/learn/sharing-state-between-components#lifting-state-up-by-example
   const [zone, setZone] = useState<string>("Europe/Amsterdam");
   return (
     <>
