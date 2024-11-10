55b2d89639d13726ad77e9266e133870e7d8b802
Alina <alina100chuk@gmail.com>
2024-11-10 14:56:20 +0100

    add readme with project description

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,7 @@ import { useState } from "react";
 import Time from "./Time";
 import TimeZone from "./TimeZone";
 
-// volgende keer maken we het mooi: https://react.fluentui.dev/?path=/docs/concepts-developer-quick-start--docs
 function App() {
-  // Leren (advanced) Dit is een voorbeeld van lifting state up, zie https://react.dev/learn/sharing-state-between-components#lifting-state-up-by-example
   const [zone, setZone] = useState<string>("Europe/Amsterdam");
   return (
     <>
