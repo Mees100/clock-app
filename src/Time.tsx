57fe778cedf8d4cfcda4d9718452c393673485d5
Alina <alina100chuk@gmail.com>
2024-11-10 14:24:56 +0100

    clean up

--- src/Time.tsx
+++ src/Time.tsx
@@ -1,12 +1,10 @@
 import { useEffect, useState } from "react";
 import { Spinner } from "@fluentui/react-components";
 
-// zie object literal type linksonder op https://www.typescriptlang.org/static/TypeScript%20Types-ae199d69aeecf7d4a2704a528d0fd3f9.png
 type CurrentTime = {
   dateTime: string;
 };
 
-// een promise, gebruik je bij asynchrone code, je hebt ook een nieuwere syntax (async/await) hiervoor maar die heb ik bewust nog even weggelaten :)
 function getTime(zone: string): Promise<CurrentTime> {
   const request = fetch(
     `https://www.timeapi.io/api/time/current/zone?timeZone=${zone}`
@@ -16,10 +14,6 @@ function getTime(zone: string): Promise<CurrentTime> {
 
 function Time({ zone }: { zone: string }) {
   const [time, setTime] = useState<CurrentTime>();
-  // deze state staat nu in App.tsx en wordt doorgegeven als prop en is hier niet meer nodig.
-  // const [zone, setZone] = useState<string>("Amsterdam");
-  console.log(time);
-
   useEffect(() => {
     setTime(undefined);
     getTime(zone).then((r) => setTime(r));
@@ -33,9 +27,6 @@ function Time({ zone }: { zone: string }) {
           Het is in {zone}: {time.dateTime}
         </span>
       )}
-      {/* <button onClick={() => setZone("London")}>London</button>
-      <button onClick={() => setZone("Amsterdam")}>Amsterdam</button>
-      <button onClick={() => setZone("Helsinki")}>Helsinki</button> */}
     </div>
   );
 }
